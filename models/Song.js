c7b7098707462073f2a1a3c1665929bbbc5c1f17
Tatiana Stewart <tstwrt7@gmail.com>
2024-02-14 13:23:59 -0500

    Defined Song Model

--- models/Song.js
+++ models/Song.js
@@ -1,7 +1,18 @@
+const { db, DataTypes, Model } = require('sequelize');
 const {Sequelize, sequelize} = require('../db');
 
 // TODO - define the Song model
-let Song;
+class Song extends Model {};
+
+Song.init({
+    title: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    length: DataTypes.INTEGER
+}, {
+    Sequelize: db,
+    modelName: "Song"
+});
+
 
 module.exports = {
     Song
